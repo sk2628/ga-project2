b85a03f4daf1848821659110e0f6ff92ed907490
sk2628 <steve.kang@iapplytics.com>
2019-11-16 16:30:22 +0800

    Server.js template created

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+
+const app = express();
+const db = mongoose.connection;
+const router = express.Router();
+const host = 3888;
+
+//MongoDB Connection
+const mongoURI = 'mongodb+srv://steve-admin:0Ptimad8@iapplytics-cluster-u4l8g.mongodb.net/test?retryWrites=true&w=majority';
+mongoose.connect(mongoURI, { useNewUrlParser: true }, () => {
+	console.log('the connection with mongod is established')
+})
+
+//Connection Logging
+db.on('error', (err) => console.log(err.message + ' is mongod not running?'))
+db.on('connected', () => console.log('mongo connected: ', mongoURI))
+db.on('disconnected', () => console.log('mongo disconnected'))
+
+//Middleware functions
+app.use(express.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+app.use(express.static('public'));
+
+//Controllers
+const dietController = require('./controllers/diets');
+app.use('/diets', dietController);
+
+//Port Listening
+app.listen(host, ()=> {
+    console.log(`I'm listening to ${host}`);
+})
